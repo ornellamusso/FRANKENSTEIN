e6a25c35710587ca89e12dd3e7232f3a4e1aabf0
LaraHV17 <lhv17gj@gmail.com>
2024-12-02 18:45:45 +0100

    add bt and binding to activities

--- app/src/main/java/com/example/eventmanager/activities/GuestListActivity.kt
+++ app/src/main/java/com/example/eventmanager/activities/GuestListActivity.kt
@@ -1,12 +1,30 @@
 package com.example.eventmanager.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import com.example.eventmanager.R
+import com.example.eventmanager.databinding.ActivityGuestAddBinding
+import com.example.eventmanager.databinding.ActivityGuestListBinding
+import com.example.eventmanager.databinding.ActivityMainBinding
 
 class GuestListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGuestListBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_list)
+        enableEdgeToEdge()
+
+        binding = ActivityGuestListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btAdd.setOnClickListener {
+            navigateToAdd()
+        }
+
+    }
+    private fun navigateToAdd() {
+        val intent = Intent(this, GuestAddActivity::class.java)
+        startActivity(intent)
     }
 }
